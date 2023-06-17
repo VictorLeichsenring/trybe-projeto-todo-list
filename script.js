91683d06d8ccb8d178846582016791554819b86c
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-17 19:06:31 -0300

    Ao clicar em um elemento da lista, a cor de fundo é alterada

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const btnCriarTarefa = document. getElementById('criar-tarefa');
 const inputTarefa = document.getElementById('texto-tarefa');
 
 //Lista de funções
+function selectLi(event) {
+    event.target.classList.add('selected');
+}
+
 function createTask () {
     const element = document.createElement('li');
     const task = inputTarefa.value;
     element.innerText = task;
     taskList.appendChild(element);
+    element.addEventListener('click', selectLi)
     inputTarefa.value = '';
 }
 
