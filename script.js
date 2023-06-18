1b9e2755650a40bbc795e31face6b0f69e947b84
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-17 21:08:30 -0300

    Criada função qu salva os dados de tarefas

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnCriarTarefa = document. getElementById('criar-tarefa');
 const inputTarefa = document.getElementById('texto-tarefa');
 const btnApagaListaTarefas = document.getElementById('apaga-tudo');
 const btnRemoverFinalizados = document.getElementById('remover-finalizados');
+const btnSalvarTarefas = document.getElementById('salvar-tarefas');
 
 //Lista de funções
 function selectLi(event) {
@@ -52,7 +53,52 @@ function removeCompletedTask(){
     }
 }
 
+function storage() {
+    const taskListChildren = taskList.children;
+    const listTasks = [];
+    for (let i = 0; i < taskListChildren.length; i++) {
+      const taskInformtion = {
+        text: taskListChildren[i].innerText,
+        completed: taskListChildren[i].classList.contains('completed'),
+        selected: taskListChildren[i].classList.contains('selected'),
+      };
+      listTasks.push(taskInformtion);
+    }
+    localStorage.setItem('tasks', JSON.stringify(listTasks));
+}
+  
+  
+
+function recovery() {
+    const listTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (listTasks) {
+      for (let i = 0; i < listTasks.length; i += 1) {
+        const itensListTask = listTasks[i];
+        const newTask = document.createElement('li');
+        newTask.innerText = itensListTask.text;
+        if (itensListTask.completed) {
+          newTask.classList.add('completed');
+        }
+        if (itensListTask.selected) {
+            newTask.classList.add('selected');
+        }
+        taskList.appendChild(newTask);
+        newTask.addEventListener('click', selectLi)
+        newTask.addEventListener('dblclick', taskCompleted);
+      }
+    }
+  }
+ 
+
+   
+
 //Lista de eventos
 btnCriarTarefa.addEventListener('click', createTask);
 btnApagaListaTarefas.addEventListener('click', clearTaskList);
 btnRemoverFinalizados.addEventListener('click', removeCompletedTask);
+btnSalvarTarefas.addEventListener('click', storage);
+
+//Recuperar dados do localStorage
+window.onload = function() {
+    recovery();
+  };
